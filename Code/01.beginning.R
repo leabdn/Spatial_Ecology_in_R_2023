b52a4b38816765e68bc8c4759146eee6fc502852
leabdn <146854903+leabdn@users.noreply.github.com>
2023-10-04 20:41:55 +0200

    Update 01.beginning.R

--- Code/01.beginning.R
+++ Code/01.beginning.R
@@ -14,4 +14,18 @@ final
 
 final^2
 
+# array
+sophi <- c(10, 20, 30, 50, 70) # microplastics 
+# fcuntions have parentheses and inseide them there are arguments
 
+paula <- c(100, 500, 600, 1000, 2000) # people
+
+plot (paula, sophi)
+plot (paula, sophi, xlab="number of people", ylab="microplastics")
+
+people <- paula
+microplastics <- sophi
+
+plot(people, microplastics, pch=19)
+plot(people, microplastics, pch=19, cex=2)
+plot(people, microplastics, pch=19, cex=2, col="blue")
