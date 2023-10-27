db8968f584a5b7ad2f7a0c56b0aae1cf6787b022
leabdn <146854903+leabdn@users.noreply.github.com>
2023-10-27 13:52:06 +0200

    Update 03.populations.distrib.R

--- Code/03.populations.distrib.R
+++ Code/03.populations.distrib.R
@@ -1,5 +1,5 @@
-# Why populations disperse over the landscape in a certain manner ?
-# Species Distribution Modelling
+# WHY POPULATIONS DISPERSE OVER THE LANDSCAPE IN A CERTAIN MANNER ?
+# SPECIES DISTRIBUTION MODELLING
 install.packages("sdm")
 install.packages("rgdal", dependencies=T)
 library(sdm)
@@ -9,63 +9,63 @@ library(rgdal) # species
 system.file("external/species.shp", package="sdm")
 file <- system.file("external/species.shp", package="sdm") # ouvrir les données .shp
 
-# Créer vecteur (coordonnées de l'espèce / l'individu)
+# CREER VECTEUR (COORDONNEES DE L'ESPECE / L'INDIVIDU)
 rana <- vect(file)  # dans terra, fonction de vecteur avec coordonnées et système de données (vect) / rana = espèce de grenouille, permet de représenter l'espèce (sa localisation)
 rana$Occurrence
 plot(rana)
 
-# Selecting presences
-# Sélectionner uniquement la présence des grenouilles (pas absence)
+# SELECTING PRESENCES Selecting presences
+# SELECTIONNER UNIQUEMENT LA PRESENCE DES GRENOUILLES (PAS ABSENCE)
 présence <- rana[rana$Occurrence== 1,]  # == veut dire égal, [] permet de séléctionner un élément (1 veut dire ici présence)
 présence # afficher caractéristiques
 plot(présence) # graphique des présences
 
-# Select absence and call them absences
+# SELECT ABSENCE AND CALL THEM ABSENCES
 absences <- rana[rana$Occurrence== 0,]
 plot(absences)
 
-# Plot presences and absences, one beside the other
+# PLOT PRESENCE AND ABSENCES, ONE BESIDE THE OTHER
 par(mfrow=c(1,2)) # Deux encadrés and 1 colonne # mfrow = multiframerow
 plot(présence)
 plot(absences) # ajouter anciennes fonctions si depuis le début
 
-# Your new friend in case of graphical nulling : 
+# YOUR NEW FRIEND IN CAS OF GRAPHICAL NULLING
 dev.off()
 
-# Plot presences and absences abse altogether with two different colour
+# PLOT PRESENCES AND ABSENCES ABSE ALTOGETHER WITH TWO DIFFERENT COLOUR
 plot(présence, col="dark blue")
 points(absences, col="light blue")
 
-# list the predictors : environmental variables
+# LIST OF PREDICTORS : ENVIRONMENTAL VARIABLES
 elev <- system.file("external/elevation.asc", package="sdm") # on ajoute un raster (même méthode que vecteur)
 elevmap <- rast(elev) # même principe que vect, avec raster
 plot(elevmap)
 
-# Ajouter points sur la carte
+# AJOUTER POINTS SUR LA CARTE
 points(présence, cex=.5)
 
-# Temperature predictor
+# TEMPERATURE PREDICTOR
 temp <- system.file("external/temperature.asc", package="sdm")
 temp # obtenir le chemin où se trouve la donnée
 tempmap <- rast(temp)
 plot(tempmap)
 points(présence, cex=.5)
 
-# Represent the vegetation cover
+# REPRESENT THE VEGETATION COVER
 veg <- system.file("external/vegetation.asc", package="sdm")
 veg
 vegmap <- rast(veg)
 plot(vegmap)
 points(présence, cex=.5) 
 
-# Represent the precipitations
+# REPRESENT THE PRECIPITATIONS
 prec <- system.file("external/precipitation.asc", package="sdm")
 prec
 precmap <- rast(prec)
 plot(precmap)
 points(présence, cex=.5) 
 
-# Multiframe : represent all plots created
+# MULTIFRAME : REPRESENT ALL PLOTS CREATED
 par(mfrow=c(2,2)) # Deux encadrés par colonne et 2 colonnes
 plot(elevmap)
 points(présence, cex=.5) # on oublie pas d'ajouter les présences à chaque graph
