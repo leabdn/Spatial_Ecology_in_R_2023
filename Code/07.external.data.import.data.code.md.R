b11ad8c903433c22bf9fc08b3f97fd6436488e34
leabdn <146854903+leabdn@users.noreply.github.com>
2023-11-26 22:04:52 +0100

    Update and rename 07.external.data.import.md.R to 07.external.data.import.data.code.R

--- Code/07.external.data.import.data.code.md.R
+++ Code/07.external.data.import.data.code.md.R
@@ -14,22 +14,22 @@
 library(terra)
 setwd("C:/Users/leabd/Documents/Erasmus courses/SPATIAL ECOLOGY IN R/07. DOWNLOAD IMAGES") # On note le chemin d'accès aux données inverse les slashs
 
-#STEP 4 : Import the data and plot the image : nomdonnéàl'imagedansterra <- rast("yourdata_in_the_working_directory.jpg")
+# STEP 4 : Import the data and plot the image : nomdonnéàl'imagedansterra <- rast("yourdata_in_the_working_directory.jpg")
 naja <- rast("najafiraq_etm_2003140_lrg - EARTH OBSERVATORY.jpg") # rast créé un SpaRaster (un raster)
-plotRGB (naja, r=1, g=2, b=3)
+plotRGB (naja, r=1, g=2, b=3) # Ici, exemple avec image de Najaf en Iraq, télécharge sur Earth Observatory
 
-#EXERCICE : DOWNLOAD THE SECOND IMAGE FROM EARTH OBSERVATORY AND PLOT IT
+# EXERCICE : DOWNLOAD THE SECOND IMAGE FROM EARTH OBSERVATORY AND PLOT IT
 library(terra)
 setwd("C:/Users/leabd/Documents/Erasmus courses/SPATIAL ECOLOGY IN R/07. DOWNLOAD IMAGES")
 najaaug <- rast("najafiraq_oli_2023219_lrg - SECOND PICTURE.jpg")
 plotRGB(najaaug, r=1, g=2, b=3)
 
-#MULTITEMPORAL CHANGE DETECTION
+# MULTITEMPORAL CHANGE DETECTION
 najadif = naja[[1]] - najaaug [[1]]
 cl - coloRampPalette ("brown", "grey", "orange")) (100)
 dev.off()
 plot(najadif, col=cl) 
 
-#EXERCICE : DOWNLOARD YOUR OWN IMAGE (LIGHTS BETWEEN FRANCESE AND ITALIAN BORDER)
+# EXERCICE : DOWNLOARD YOUR OWN IMAGE (LIGHTS BETWEEN FRANCESE AND ITALIAN BORDER)
 lights <- rast("ISS023-E-029061_lrg.jpg")
 plotRGB(lights, r=1, g=2, b=3)
