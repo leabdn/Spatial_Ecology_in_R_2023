5e41d5a3a0d822aeebae03d3b82e39a6ae641329
leabdn <146854903+leabdn@users.noreply.github.com>
2023-12-09 19:23:38 +0100

    Update 09.Classification.temp.R

--- Code/09.Classification.temp.R
+++ Code/09.Classification.temp.R
@@ -1,55 +1,53 @@
-# Classifying satellite images and estimate the amount of change
-
+# CLASSIFYING SATELLITE IMAGES AND ESTIMATE THE AMOUNT OF CHANGE
+# 1. OPEN PACKAGES
 library(terra)
 library(imageRy)
 library(ggplot2)
-
+library(patchwork)
 im.list()
-
 # https://www.esa.int/ESA_Multimedia/Images/2020/07/Solar_Orbiter_s_first_views_of_the_Sun6
 # additional images: https://webbtelescope.org/contents/media/videos/1102-Video?Tag=Nebulas&page=1
 
+# 2. IMPORT THE DATA
 sun <- im.import("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 
+# 3. CLASSIFY SATELLITE DATA
+sunc <- im.classify(sun)
+plotRGB(sun, 1, 2, 3)
+plot(sunc)
 sunc <- im.classify(sun, num_clusters=3)
-
-# classify satellite data
-
 im.list()
-
 m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")
 m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
-  
 m1992c <- im.classify(m1992, num_clusters=2)                    
 plot(m1992c)
 # classes: forest=1; human=2
-
 m2006c <- im.classify(m2006, num_clusters=2)
 plot(m2006c)
 # classes: forest=1; human=2
 
+# 4. PLOT COMPARATIVE DATA (DIFFERENTS DATES)
 par(mfrow=c(1,2))
 plot(m1992c[[1]])
 plot(m2006c[[1]])
-
 f1992 <- freq(m1992c)
 f1992
 tot1992 <- ncell(m1992c)
-# percentage
+
+# 5. COMPUTE PERCENTAGES
 p1992 <- f1992 * 100 / tot1992 
 p1992
 # forest: 83%; human: 17%
 
-# percentage of 2006
 f2006 <- freq(m2006c)
 f2006
 tot2006 <- ncell(m2006c)
-# percentage
+
 p2006 <- f2006 * 100 / tot2006 
 p2006
 # forest: 45%; human: 55%
 
-# building the final table
+# 6. BUILDING THE FINAL TABLE
 class <- c("forest", "human")
 y1992 <- c(83, 17)
 y2006 <- c(45, 55) 
@@ -57,49 +55,16 @@ y2006 <- c(45, 55)
 tabout <- data.frame(class, y1992, y2006)
 tabout
 
-# final output
+# 7. FINAL OUTPUT
 cover <- c("forest", "human")
 perc1992 <- c(83, 17)
 p1 <- ggplot(tabout, aes(x=cover, y=perc1992, color=cover)) + geom_bar(stat="identity", fill="white")
 
-# Procedure for classifying remote sensing data
 
-library(terra)
-library(imageRy)
-library(ggplot2)
-library(patchwork)
+# /////////////////// Procedure for classifying remote sensing data
 
-im.list()
-
-sun <- im.import("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
-
-sunc <- im.classify(sun)
-
-plotRGB(sun, 1, 2, 3)
-plot(sunc)
-
-#-----
-
-im.list()
-
-# Import the data
-m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")
-m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
-
-# Classification of 1992
-m1992c <- im.classify(m1992, num_clusters=2)
-
-# classes of 1992:
-# class 1: agricultural areas
-# class 2: forest
-
-#2006
-
-m2006c <- im.classify(m2006, num_clusters=2)
-
-# classes of 2006:
-# class 1: agricultural areas
-# class 2: forest
+# classes of 1992 : class 1: agricultural areas, class 2: forest
+# classes of 2006 : class 1: agricultural areas, class 2: forest
 
 # final estimates
 # 1992
@@ -116,7 +81,6 @@ perc1992 = freq1992 * 100 / tot1992
 perc1992
 
 # 1992: forest = 83.08%, agriculture = 16.91%
-
 tot2006 = ncell(m2006)
 perc2006 = freq2006 * 100 / tot2006
 perc2006
