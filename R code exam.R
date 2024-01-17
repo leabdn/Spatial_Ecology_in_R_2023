c460ad6026fe1bbdfe551fce652064b4423791f1
leabdn <146854903+leabdn@users.noreply.github.com>
2024-01-17 22:47:02 +0100

    Update R code exam.R

--- R code exam.R
+++ R code exam.R
@@ -22,8 +22,8 @@ ndvi2021 = (jpg2021[[1]] - jpg2021[[2]]) / (jpg2021[[1]] + jpg2021[[2]]) # (NIR-
 c <- colorRampPalette(c("darkolivegreen1", "chartreuse", "chartreuse2", "darkgreen"))(100)
 
 # CALCULATION OF THE NDVI FOR 2023
-jpg2023 <- rast(".jpg")
-ndvi20213 = (jpg2023[[1]] - jpg2023[[2]]) / (jpg2023[[1]] + jpg2023[[2]]) # (NIR-RED) / (NIR+RED)
+jpg2023 <- rast("2023-07-22-00_00_2023-07-22-23_59_Sentinel-2_L1C_False_color.jpg")
+ndvi2023 = (jpg2023[[1]] - jpg2023[[2]]) / (jpg2023[[1]] + jpg2023[[2]]) # (NIR-RED) / (NIR+RED)
 
 # PLOT AND COMPARE THE NDVI FOR THE TWO PERIODS
 par(mfrow=c(2,1))
@@ -32,13 +32,11 @@ plot(ndvi2023, col=c)
 dev.off()
 
 # CALCULATION OF THE NDWI FOR 2021
-ndwi2021 = (jpg[[3]] - jpg[[1]]) / (jpg[[3]] + jpg[[1]]) # NDWI (McFEETERS) = (Green-NIR) / (Green+NIR)
+ndwi2021 = (jpg2021[[3]] - jpg2021[[1]]) / (jpg2021[[3]] + jpg2021[[1]]) # NDWI (McFEETERS) = (Green-NIR) / (Green+NIR)
 c2 <- colorRampPalette(c("darkslategray1", "cyan4", "blue", "darkblue")) (100)
-
 # CALCULATION OF THE NDWI FOR 2023
-ndwi2031 = (jpg2023[[3]] - jpg2023[[1]]) / (jpg2023[[3]] + jpg2023[[1]]) # NDWI (McFEETERS) = (Green-NIR) / (Green+NIR)
-
-# PLOT AND COMPARE THE NDVI FOR THE TWO PERIODS
+ndwi2023 = (jpg2023[[3]] - jpg2023[[1]]) / (jpg2023[[3]] + jpg2023[[1]]) # NDWI (McFEETERS) = (Green-NIR) / (Green+NIR)
+# PLOT AND COMPARE THE NDWI FOR THE TWO PERIODS
 par(mfrow=c(2,1))
 plot(ndwi2021, col = c2)
 plot(ndwi2023, col = c2)
@@ -47,11 +45,9 @@ dev.off()
 # CALCULATION OF THE CI FOR 2021
 ci2021 = (jpg2021[[2]] - jpg2021[[3]]) / (jpg2021[[2]] + jpg2021[[3]]) # CI = (Red-Green) / (Red+Green)
 c3 <- colorRampPalette(c("white", "azure", "darkslategray", "black")) (100)
-
 # CALCULATION OF THE CI FOR 2023
 ci2023 = (jpg2023[[2]] - jpg2023[[3]]) / (jpg2023[[2]] + jpg2023[[3]]) # CI = (Red-Green) / (Red+Green)
-
-# PLOT AND COMPARE THE NDVI FOR THE TWO PERIODS
+# PLOT AND COMPARE THE CI FOR THE TWO PERIODS
 par(mfrow=c(2,1))
 plot(ci2023, col = c3)
 plot(ci2023, col = c3)
@@ -59,22 +55,17 @@ dev.off()
 
 -------------------------------------------------------------------------------------------------------------------------------
 ## TO GO FURTHER
-# CUTTING THE CALOPTERYX VIRGO DATA AT THE INDRE-ET-LOIRE SCALE
-
-# CUTTING AND ADD THE OCCURENCES OF CALOPTERYX VIRGO ON THE RASTER OBTAINED
-# OPEN THE C_virgo.shp FILE # Terra packages
-C_virgo <- vect(file) # Insert the shape file
-
-# CUTTING
+# CUTTING THE CALOPTERYX VIRGO DATA AT THE RASTER OBTAINED
 install.packages("ggplot2")
 install.packages("sf")
 install.packages("terra")
 library(ggplot2)
 library(sf)
 library(terra)
+c_virgo <- vect(file) # Insert the shape file
 c_virgo # Have the extent to compute below
 ndvi <- rast("NDVI.png")
-ndvicrop <- crop(ndvi, extent(46.42594, 48.70883, 0.13071, 3.01558)) # Extent of the .shp file (Data of Calopteryx Virgo)
+ndvicrop <- crop(ndvi, extent(0.17476, 0.91719, 46.91004, 47.48635)) # Extent of the .shp file (Data of Calopteryx Virgo)
 datframe_ndvi <- as.data.frame(ndvicrop, xy = TRUE)
 plot(ndvicrop)
 points(C_virgo, cex =.5)
